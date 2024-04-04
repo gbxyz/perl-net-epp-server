eedfb75dbe255037875d1e4205fa78c3913702e6
Gavin Brown <gavin.brown@icann.org>
2024-04-04 15:20:06 +0100

    define default messages for each result code

--- lib/Net/EPP/Server.pm
+++ lib/Net/EPP/Server.pm
@@ -22,8 +22,46 @@ use bytes;
 use utf8;
 use open qw(:encoding(utf8));
 use feature qw(state);
+use vars qw(%MESSAGES);
 use strict;
 use warnings;
+
+our %MESSAGES = (
+    1000 => 'Command completed successfully.',
+    1001 => 'Command completed successfully; action pending.',
+    1300 => 'Command completed successfully; no messages.',
+    1301 => 'Command completed successfully; ack to dequeue.',
+    1500 => 'Command completed successfully; ending session.',
+    2000 => 'Unknown command.',
+    2001 => 'Command syntax error.',
+    2002 => 'Command use error.',
+    2003 => 'Required parameter missing.',
+    2004 => 'Parameter value range error.',
+    2005 => 'Parameter value syntax error.',
+    2100 => 'Unimplemented protocol version.',
+    2101 => 'Unimplemented command.',
+    2102 => 'Unimplemented option.',
+    2103 => 'Unimplemented extension.',
+    2104 => 'Billing failure.',
+    2105 => 'Object is not eligible for renewal.',
+    2106 => 'Object is not eligible for transfer.',
+    2200 => 'Authentication error.',
+    2201 => 'Authorization error.',
+    2202 => 'Invalid authorization information.',
+    2300 => 'Object pending transfer.',
+    2301 => 'Object not pending transfer.',
+    2302 => 'Object exists.',
+    2303 => 'Object does not exist.',
+    2304 => 'Object status prohibits operation.',
+    2305 => 'Object association prohibits operation.',
+    2306 => 'Parameter value policy error.',
+    2307 => 'Unimplemented object service.',
+    2308 => 'Data management policy violation.',
+    2400 => 'Command failed.',
+    2500 => 'Command failed; server closing connection.',
+    2501 => 'Authentication error; server closing connection.',
+    2502 => 'Session limit exceeded; server closing connection.',
+);
 
 =pod
 
@@ -932,16 +970,20 @@ change its contents as needed.
 sub generate_response {
     my $self    = shift;
     my %args    = @_;
+
     my $clTRID  = $args{'clTRID'};
     my $svTRID  = $args{'svTRID'};
+
     my $code    = $args{'code'} || OK;
-    my $msg     = $args{'msg'} || ($code < UNKNOWN_COMMAND ? 'Command completed successfully.' : 'Command failed.');
+    my $msg     = $args{'msg'} || $MESSAGES{$code} || ($code < UNKNOWN_COMMAND ? $MESSAGES{OK} : $MESSAGES{COMMAND_FAILED});
 
     my $frame = XML::LibXML::Document->new;
 
     $frame->setDocumentElement($frame->createElementNS($Net::EPP::Frame::EPP_URN, 'epp'));
     my $response = $frame->documentElement->appendChild($frame->createElement('response'));
+
     my $result = $response->appendChild($frame->createElement('result'));
+
     $result->setAttribute('code', $code);
     $result->appendChild($frame->createElement('msg'))->appendText($msg);
 
@@ -966,8 +1008,8 @@ unspecified reasons.
 
 sub generate_error {
     my ($self, %args) = @_;
-    $args{'code'} = $args{'code'} || COMMAND_FAILED;
-    $args{'msg'} = $args{'msg'} || 'An internal error occurred. Please try again later.';
+    $args{'code'} ||= COMMAND_FAILED;
+    $args{'msg'} = $args{'msg'} || $MESSAGES{$args{'code'}} || 'An internal error occurred. Please try again later.';
     return $self->generate_response(%args);
 }
 
