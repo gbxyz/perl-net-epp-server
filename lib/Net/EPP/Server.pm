4b22731972115bf052a876955053e22c538c5cec
Gavin Brown <gavin.brown@fastmail.uk>
2025-02-19 10:37:43 +0000

    updated

--- lib/Net/EPP/Server.pm
+++ lib/Net/EPP/Server.pm
@@ -156,7 +156,7 @@ Provisioning Protocol (EPP)|https://www.rfc-editor.org/info/std69> servers.
 
 It implements the TLS/TCP transport described in L<RFC 5734|https://www.rfc-editor.org/info/rfc5734>,
 and the L<EPP Server State Machine|https://www.rfc-editor.org/rfc/rfc5730.html#:~:text=Figure%201:%20EPP%20Server%20State%20Machine>
-described in L<Section 2 of RFC 5730|https://www.rfc-editor.org/rfc/rfc5730.html>.
+described in L<Section 2 of RFC 5730|https://www.rfc-editor.org/rfc/rfc5730.html#section-2>.
 
 =cut
 
